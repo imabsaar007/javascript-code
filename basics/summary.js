e63ad14e14cfffce8dee4b06844639f74dbfe75b
Md Absaar <128820019+imabsaar007@users.noreply.github.com>
2024-07-01 18:24:01 +0000

    summary updated

---
+++ basics/summary.js
@@ -0,0 +1,10 @@
+// Primitive
+// 7 Types :- String,Number,Boolean,Undefined,Null,Symbol,BigInt
+
+//Reference (Non-Primitive)
+// Array,Object,Function
+
+const id = Symbol('134') // Symbol always resembles unique elements
+const Anotherid = Symbol('134') // These two values cannot be same
+
+console.log(id===Anotherid)
