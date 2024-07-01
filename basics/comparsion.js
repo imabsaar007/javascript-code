16f63e1ef390bd30581ce72d830d05d9250c6c50
Md Absaar <128820019+imabsaar007@users.noreply.github.com>
2024-07-01 18:12:03 +0000

    comparisons notes added

---
+++ basics/comparsion.js
@@ -0,0 +1,3 @@
+console.log("2">1) // will convert "2" into integer internally to compare the two entities
+console.log("2"===2) // This will show false because "2" is a string and === operator first check data type and then compare it with the given value.
+
