ee0fdda65d4ad7c678ad3b7d12a74040c00d2793
Md Absaar <128820019+imabsaar007@users.noreply.github.com>
2024-07-01 16:58:56 +0000

    datatypes and let var const updated

---
+++ basics/second.js
@@ -0,0 +1,6 @@
+const accountmail = "imabsaar007@gmail.com"  // It is constant as it cannot be changed.
+let accountId = 12123
+var accountCode = "101010" // Var should Not Be used because of it's issue in block scope or Functional Scope
+accountCity = "Asansol"
+
+console.table([accountmail,accountId,accountCode]) //Prints the data in tabular manner.
---
+++ basics/third.js
@@ -0,0 +1,7 @@
+let name = "Absaar" // String datatype
+let age = 23 // Integer Data Type
+let state = null
+
+// Null=> Standalone Value
+// Undefined => Data type of undefined is Undefined
+
