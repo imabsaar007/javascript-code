ed076d29e37cb60331e8f3a06bf71bf9aa2733c0
Md Absaar <128820019+imabsaar007@users.noreply.github.com>
2024-07-01 17:14:14 +0000

    conversion Techniques

---
+++ basics/conversion.js
@@ -0,0 +1,14 @@
+// let age = "33"
+// console.log(typeof(age))
+// let ConvNum = Number(age)
+// console.log(typeof(ConvNum))
+
+let num = "33abc"
+console.log(typeof num)
+let cv = Number(num)
+console.log(typeof cv) // it will type cast it but there is some issue 
+console.log(cv) // It will show not a number as it is an invalid argument even after typecasting it
+// Conversion Techniques With Boolean
+// 1=> True ; 0=> False;
+// ""=> False; "Anything"=> True;
+//
